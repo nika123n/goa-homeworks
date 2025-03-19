2e2db3983d7356a22a7e5cca43e610627c0eb52d
nika123n <nikoloznikoloz234@gmail.com>
2025-03-19 22:00:06 +0400

    commit

---
+++ day 79/classwork/script.js
@@ -0,0 +1,14 @@
+// შექმენით საიტი სადაც გამოყენებთ prompt()-ს, alert()-ს, confirm()-ს, შედარების ოპერატორებს, ასევე confirm()-ში მომხმარებლის არევანი შეინახეთ ცვლადში და გამოიტანეთ კონსოლში.
+
+let number = prompt('enter number: ')
+let number2 = prompt('enter number: ')
+
+console.log(number > number2)
+console.log(number2 > number)
+console.log(number2 = number)
+
+alert('hallo my friend')
+
+let user = confirm('write word: ')
+
+console.log(user)
---
+++ day 79/homework/script.js
@@ -0,0 +1,23 @@
+// შექმენით საიტი:
+// * სადაც გამოიყენებთ ყველა ნასწავლ ინფორმაციის ტიპს, შეინახეთ ისინი ცვლადებში, შემდეგ კი იმუშავეთ მათზე, შეკრიბეთ ა.შ,
+// * საიტზე ასევე გამოიყენეთ ყველა შედარების ოპერატორი და ორი რიცხვის შედარების შედეგები გამოიტანეთ კონსოლში,
+// * საბოლოოდ, დაამატეთ confirm() და მომხმარებელს კითხეთ სურს თუ არა საიტის დატოვება, თუ პასუხი დადებითი იქნება კონსოლში გამოუტანეთ 'you will live the site soon', თუ პასუხი  უარყოფითი იქნება კონსოლში გამოუტანეთ 'you are staying on the site'.
+
+let number = 15
+let number2 = 57
+
+console.log(number - number2)
+console.log(number2 - number)
+console.log(number2 + number)
+console.log(number2 * number)
+console.log(number2 / number)
+
+let input = alert(prompt("gindat saitis datoveba: "))
+
+if(input = "ki"){
+    console.log('you will live the site soon')
+    
+}
+if(input = "ara"){
+    console.log('you are staying on the site')
+}
